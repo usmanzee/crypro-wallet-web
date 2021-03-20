3be07323247884a05aaf18f5831c130df9e12502
Muhammad <usman.jamil0308@gmail.com>
2021-03-20 17:29:07 +0500

    swap screen wallet matching with markets fix

--- src/screens/SwapScreen/index.tsx
+++ src/screens/SwapScreen/index.tsx
@@ -51,6 +51,13 @@ import {
     MemberLevels,
     memberLevelsFetch,
     selectMemberLevels,
+    Market,
+    marketsFetch,
+    selectMarkets,
+    selectMarketTickers,
+    orderExecuteFetch,
+    selectOrderExecuteLoading,
+    marketsTickersFetch,
 } from '../../modules';
 
 interface ReduxProps {
@@ -62,6 +69,13 @@ interface ReduxProps {
     exchangeSuccess: boolean;
     exchangeLoading: boolean;
     memberLevels?: MemberLevels;
+    markets: Market[];
+    executeLoading: boolean;
+    marketTickers: {
+        [key: string]: {
+            last: string;
+        },
+    };
 }
 
 interface DispatchProps {
@@ -70,6 +84,9 @@ interface DispatchProps {
     exchangeRateReset: typeof exchangeRateReset;
     exchangeRequest: typeof exchangeRequest;
     memberLevelsFetch: typeof memberLevelsFetch;
+    fetchMarkets: typeof marketsFetch;
+    tickers: typeof marketsTickersFetch;
+    orderExecute: typeof orderExecuteFetch;
 }
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -162,12 +179,13 @@ const SwapComponent = (props: Props) => {
     const WAIT_INTERVAL = 1000;
     //Props
     const classes = useStyles();
-    const { wallets, user: { level, otp }, memberLevels, isFetchingRate, exchangeRate, exchangeSuccess, exchangeLoading} = props;
+    const { wallets, markets, user: { level, otp }, memberLevels, isFetchingRate, exchangeRate, exchangeSuccess, exchangeLoading, executeLoading, marketTickers} = props;
 
     //States
     const [walletsFromCurrency, setWalletsFromCurrency] = React.useState<string>(defaultWalletsFromCurrency);
     const [walletsToCurrency, setWalletsToCurrency] = React.useState<string>(defaultWalletsToCurrency);
     const [walletsFromLoading, setWalletsFromLoading] = React.useState<boolean>(true);
+    const [filteredWallets, setFilteredWallets] = React.useState<WalletItemProps[]>([]);
     const [walletsFrom, setWalletsFrom] = React.useState<WalletItemProps[]>([]);
     const [walletsToLoading, setWalletsToLoading] = React.useState<boolean>(true);
     const [walletsTo, setWalletsTo] = React.useState<WalletItemProps[]>([]);
@@ -196,40 +214,92 @@ const SwapComponent = (props: Props) => {
     }, [wallets]);
 
     React.useEffect(() => {
+        if (!markets.length) {
+            props.fetchMarkets();
+        }
+    }, [markets]);
+
+    React.useEffect(() => {
+        if (!markets.length) {
+            props.tickers();
+        }
+    }, [markets]);
+
+    React.useEffect(() => {
         if (!memberLevels) {
              props.memberLevelsFetch();
          }
      }, [memberLevels]);
 
     React.useEffect(() => {
-        if (wallets.length && !walletsFrom.length) {
-            setWalletsFrom(wallets)
-        } else if(walletsFrom.length > 0 && wallets.length > 0) {
-            
-            let searchedOption = searchCurrencyInWallets(walletsFromCurrency);
-            setSelectedWalletFromOption(searchedOption);
+        if (wallets.length && markets.length) {
+            let filteredData = [];
+            filteredData = wallets.filter(function(wallet) {
+                var exists = markets.some(function(market) {
+                    return wallet.currency == market.base_unit || wallet.currency == market.quote_unit; 
+                });
+                return exists ? true : false;
+            });
+            setFilteredWallets(filteredData);
         }
-    }, [wallets, walletsFrom]);
+      
+     }, [wallets, markets]);
 
     React.useEffect(() => {
-        if (wallets.length && !walletsTo.length) {
-            setWalletsTo(wallets)
-        } else if(walletsTo.length > 0 && wallets.length > 0) {
+        if (filteredWallets.length && !walletsFrom.length) {
+            setWalletsFrom(filteredWallets)
+        } else if(walletsFrom.length > 0 && filteredWallets.length > 0) {
             
-            let searchedOption = searchCurrencyInWallets(walletsToCurrency);
-            setSelectedWalletToOption(searchedOption);
+            // let searchedOption = searchCurrencyInWallets(walletsFromCurrency);
+            setSelectedWalletFromOption(filteredWallets[0]);
         }
-    }, [wallets, walletsTo]);
+    }, [filteredWallets, walletsFrom]);
+
+    // React.useEffect(() => {
+    //     if (filteredWallets.length && !walletsTo.length) {
+    //         setWalletsTo(filteredWallets)
+    //     } else if(walletsTo.length > 0 && filteredWallets.length > 0) {
+            
+    //         // let searchedOption = searchCurrencyInWallets(walletsToCurrency);
+    //         setSelectedWalletToOption(filteredWallets[1]);
+    //     }
+    // }, [filteredWallets, walletsTo]);
 
     React.useEffect(() => {
         if(selectedWalletFromOption) {
             setWalletsFromLoading(false);
+            var matchingWallets = searchToWalletsOptionsWithMactingMarkets();
+            setWalletsTo(matchingWallets);
+            setSelectedWalletToOption(matchingWallets[0]);
         }
         setWalletsFromCurrency(selectedWalletFromOption ? selectedWalletFromOption.currency : walletsFromCurrency);
         checkWalletsFromSelectedOption();
         getExchangeRates();
         handleWalletsFromAmountErrors(walletsFromAmount);
     }, [selectedWalletFromOption])
+
+    const currenciesList : string[] = [];
+
+    const searchToWalletsOptionsWithMactingMarkets  = () => {
+        var marketsList = markets.filter(function(market){
+            return selectedWalletFromOption ? selectedWalletFromOption.currency.toLowerCase() === market.base_unit.toLowerCase() || selectedWalletFromOption.currency.toLowerCase() === market.quote_unit.toLowerCase() : [];
+        });
+
+        for (var i in marketsList) {
+            if (selectedWalletFromOption && marketsList[i].base_unit.toLowerCase() !=
+            selectedWalletFromOption.currency.toLowerCase()) {
+                currenciesList.push(marketsList[i].base_unit.toLowerCase());
+            }
+            if (selectedWalletFromOption && marketsList[i].quote_unit.toLowerCase() !=
+            selectedWalletFromOption.currency.toLowerCase()) {
+                currenciesList.push(marketsList[i].quote_unit.toLowerCase());
+            }
+          }
+          var newList = filteredWallets.filter(function(wallet) {
+            return currenciesList.includes(wallet.currency.toLowerCase());
+          });
+          return newList;
+    }
 
     React.useEffect(() => {
         if(selectedWalletToOption) {
@@ -271,6 +341,12 @@ const SwapComponent = (props: Props) => {
             resetFrom();
         }
     }, [exchangeSuccess])
+
+    React.useEffect(() => {
+        if(executeLoading) {
+            resetFrom();
+        }
+    }, [executeLoading])
 
     const selectedWalletFromCurrency: string = selectedWalletFromOption && selectedWalletFromOption.currency ? selectedWalletFromOption.currency : defaultWalletsFromCurrency;
     const selectedWalletToCurrency: string = selectedWalletToOption && selectedWalletToOption.currency ? selectedWalletToOption.currency : defaultWalletsToCurrency;
@@ -428,13 +504,40 @@ const SwapComponent = (props: Props) => {
         return (walletsFromAmount && walletsFromError) || (otpCode && otpCodeError) || exchangeLoading;
     }
     const handleSwapButtonClick = async () => {
-        const requestData = {
-            base_currency: selectedWalletToCurrency,
-            qoute_currency: selectedWalletFromCurrency,
-            qoute_amount: walletsFromAmount,
-            otp: otpCode
-        };
-        props.exchangeRequest(requestData);
+        if(selectedWalletFromOption && selectedWalletFromOption.type == 'fiat' && selectedWalletToOption && selectedWalletToOption.type == 'fiat') {
+            const requestData = {
+                base_currency: selectedWalletToCurrency,
+                qoute_currency: selectedWalletFromCurrency,
+                qoute_amount: walletsFromAmount,
+                otp: otpCode
+            };
+            props.exchangeRequest(requestData);
+        } else {
+            var orderSide = 'sell';
+            var existingMarket;
+            existingMarket =  markets.find(function(market) {
+                return  market.base_unit.toLowerCase() == selectedWalletFromCurrency &&
+                market.quote_unit.toLowerCase() == selectedWalletToCurrency
+            });
+            if(!existingMarket) {
+                orderSide = 'buy';
+                existingMarket =  markets.find(function(market) {
+                    return  market.quote_unit.toLowerCase() == selectedWalletFromCurrency &&
+                    market.base_unit.toLowerCase() == selectedWalletToCurrency
+                });
+            }
+            const currentTicker = marketTickers[existingMarket.id];
+
+    
+            const requestData = {
+                volume: walletsFromAmount,
+                market: existingMarket.id,
+                price: currentTicker.last,
+                side: orderSide,
+                ord_type: 'limit',
+            };
+            props.orderExecute(requestData);
+        }
     }
 
     const resetFrom = () => {
@@ -748,9 +851,9 @@ const SwapComponent = (props: Props) => {
                             fullWidth={true}
                             onClick={handleSwapButtonClick}
                             // disabled={isValidForm()}
-                            disabled={Number(walletsFromAmount) <= 0 || Boolean(otpCode.length != 6) || walletsFromError || exchangeLoading}
+                            disabled={Number(walletsFromAmount) <= 0 || Boolean(otpCode.length != 6) || walletsFromError || exchangeLoading || executeLoading}
                         >
-                            {exchangeLoading ? <CircularProgress className={classes.buttonProgress} size={18} /> : <FormattedMessage id={'page.body.swap.button.text.buy'} />}
+                            {executeLoading || exchangeLoading ? <CircularProgress className={classes.buttonProgress} size={18} /> : <FormattedMessage id={'page.body.swap.button.text.buy'} />}
                         </Button>
                     </>
                 }
@@ -783,12 +886,15 @@ const SwapComponent = (props: Props) => {
 const mapStateToProps = (state: RootState): ReduxProps => ({
     user: selectUserInfo(state),
     wallets: selectWallets(state),
+    markets: selectMarkets(state),
+    marketTickers: selectMarketTickers(state),
     walletsLoading: selectWalletsLoading(state),
     isFetchingRate: selectIsFetchingExchangeRate(state),
     exchangeRate: selectExchangeRate(state),
     exchangeSuccess: selectExchangeSuccess(state),
     exchangeLoading: selectExchangeLoading(state),
     memberLevels: selectMemberLevels(state),
+    executeLoading: selectOrderExecuteLoading(state),
 });
 const mapDispatchToProps = dispatch => ({
     fetchWallets: () => dispatch(walletsFetch()),
@@ -800,6 +906,9 @@ const mapDispatchToProps = dispatch => ({
     },
     exchangeRateReset: () => dispatch(exchangeRateReset()),
     memberLevelsFetch: () => dispatch(memberLevelsFetch()),
+    fetchMarkets: () => dispatch(marketsFetch()),
+    tickers: () => dispatch(marketsTickersFetch()),
+    orderExecute: payload => dispatch(orderExecuteFetch(payload)),
 });
 
 export const SwapScreen = injectIntl(connect(mapStateToProps, mapDispatchToProps)(SwapComponent))
